a8a96acae8f6ea4d50f7c1f609cc04bdec43f9c6
BekzodGayratov <bekzodflutter@gmail.com>
2022-09-21 15:53:42 +0500

    Localization service was acitvated

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:responsive/core/constants/theme.dart';
 import 'package:responsive/cubit/auth/sign_in/sign_in_state.dart';
 import 'package:responsive/cubit/home/user_cubit.dart';
@@ -9,10 +11,21 @@ import 'package:responsive/router/router.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(MultiBlocProvider(providers: [
-    BlocProvider(create: (context) => UserCubit()),
-    BlocProvider(create: (context) => SignInCubit()),
-  ], child: MyApp()));
+  await EasyLocalization.ensureInitialized();
+  await setDefaultLang();
+  runApp(MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => UserCubit()),
+        BlocProvider(create: (context) => SignInCubit()),
+      ],
+      child: EasyLocalization(
+          saveLocale: true,
+          child: MyApp(),
+          supportedLocales: const [
+            Locale('uz'),
+            Locale('en'),
+          ],
+          path: "lib/lang")));
 }
 
 class MyApp extends StatelessWidget {
@@ -21,10 +34,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      locale: context.locale,
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
       debugShowCheckedModeBanner: false,
       theme: NafTheme.them,
       initialRoute: 'splash',
       onGenerateRoute: RouterGenerator.router.onGenerate,
     );
+  }
+}
+
+setDefaultLang() async {
+  if (GetStorage().read('lang') == null) {
+    await GetStorage().write('lang', 'uz');
   }
 }
